a52e952761e0665a73af10d47fe8db242e329b89
Vinuka Wathsara <vinukawathsara@gmail.com>
2020-10-13 00:05:57 +0530

    Account delete and feedback

--- app/src/main/java/com/example/easypc/ProfileActivity.java
+++ app/src/main/java/com/example/easypc/ProfileActivity.java
@@ -1,5 +1,6 @@
 package com.example.easypc;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,9 +9,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -22,9 +27,12 @@ public class ProfileActivity extends AppCompatActivity {
 
     private TextView proName,proAddress,proEmail,proContact;
     private Button proEdit,home;
+    private Button proDel;
+    private Button feedback;
 
     private FirebaseAuth auth;
     private FirebaseDatabase fdb;
+    private FirebaseUser fbu;
 
 
     @Override
@@ -38,10 +46,13 @@ public class ProfileActivity extends AppCompatActivity {
         proEmail = findViewById(R.id.profileemail);
         proContact = findViewById(R.id.profilecontact);
         proEdit = findViewById(R.id.profileedit);
+        proDel = findViewById(R.id.profiledelete);
+        feedback = findViewById(R.id.feedback);
         home = findViewById(R.id.home);
 
         auth = FirebaseAuth.getInstance();
         fdb = FirebaseDatabase.getInstance();
+        fbu = auth.getCurrentUser();
 
         DatabaseReference databaseReference = fdb.getReference(auth.getUid());
 
@@ -66,6 +77,53 @@ public class ProfileActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(ProfileActivity.this,UpdateProfile.class));
+            }
+        });
+
+        feedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ProfileActivity.this,feedback.class));
+            }
+        });
+
+        proDel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final AlertDialog.Builder dialog = new AlertDialog.Builder(ProfileActivity.this);
+                dialog.setTitle("Are you sure?");
+                dialog.setMessage("Deleting this account will result in completely removing your " +
+                        "account from the system and you won't be able to access the app.");
+                dialog.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        fbu.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()){
+                                    Toast.makeText(ProfileActivity.this, "Account deleted", Toast.LENGTH_LONG).show();
+                                    Intent intent = new Intent(ProfileActivity.this,FirstActivity.class);
+                                    startActivity(intent);
+
+                                }else{
+                                    Toast.makeText(ProfileActivity.this,task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+
+                            }
+                        });
+
+                    }
+                });
+
+                dialog.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                AlertDialog alertDialog = dialog.create();
+                alertDialog.show();
+
             }
         });
 
